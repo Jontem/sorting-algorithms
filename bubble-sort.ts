6bd02e45e65e42b202595d1cc5cf68b86ba3db7d
Jonathan Mourtada <jonathan.mourtada@divid.se>
2022-11-17 08:42:45 +0100

    optimized bubble sort

--- bubble-sort.ts
+++ bubble-sort.ts
@@ -1,9 +1,18 @@
-const arr = [10, 9, 1, 4, 1, 2, 6, 8, 3];
-sort(arr);
-console.log(arr);
+const arr = [11, 10, 3, 4, 1, 7, 9, 2];
+console.log(sort(arr.concat(), "simple"));
+console.log(sort(arr.concat(), "optimized"));
 
-export function sort(arr: Array<number>): void {
-  bubbleSort(arr);
+export function sort(
+  arr: Array<number>,
+  optimized: "simple" | "optimized"
+): Array<number> {
+  if (optimized === "simple") {
+    bubbleSort(arr);
+    return arr;
+  } else {
+    optimizedBubbleSort(arr);
+    return arr;
+  }
 }
 
 function bubbleSort(arr: Array<number>): void {
@@ -16,6 +25,23 @@ function bubbleSort(arr: Array<number>): void {
         swapped = true;
       }
     }
+  } while (swapped);
+}
+
+function optimizedBubbleSort(arr: Array<number>): void {
+  let swapped;
+  let n = arr.length;
+  do {
+    swapped = false;
+    let newN = 0;
+    for (let i = 0; i < n; i++) {
+      if (arr[i - 1] > arr[i]) {
+        swap(arr, i - 1, i);
+        swapped = true;
+        newN = i;
+      }
+    }
+    n = newN;
   } while (swapped);
 }
 
